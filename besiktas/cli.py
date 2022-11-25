4ceab05f7c3b6157afab01299a83ae35eb323af7
osman.kose <osmanuygar.kose@sahibinden.com>
2022-11-25 04:53:57 +0300

    corrected argument parser

--- besiktas/cli.py
+++ besiktas/cli.py
@@ -1,11 +1,12 @@
-from .screen import Application, Footballers
+from .screen import Application, Footballers, Stats
 import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Besiktas ClI')
-    parser.add_argument('kadro', nargs='?', const='foo')
+    parser.add_argument('-kadro', action='store_true', help='Besiktas Squad')
+    parser.add_argument('-stats', action='store_true', help='Top Scorers')
     args = parser.parse_args()
-    if args.kadro is None:
+    if args.kadro is False and args.stats is False:
         try:
             screen = Application()
             screen.setup()
@@ -15,9 +16,19 @@ def main():
         except Exception as ex:
             print(ex)
 
-    else:
+    if args.kadro:
         try:
             screen = Footballers()
+            screen.setup()
+            screen.run()
+        except KeyboardInterrupt:
+            pass
+        except Exception as ex:
+            print(ex)
+
+    if args.stats:
+        try:
+            screen = Stats()
             screen.setup()
             screen.run()
         except KeyboardInterrupt:
